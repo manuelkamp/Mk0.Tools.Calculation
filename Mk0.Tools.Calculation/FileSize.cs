a21f35bea2692be983a2697eeb44456b953b9066
Manuel Kamper <manuelk@outlook.com>
2019-03-22 23:02:35 +0100

    Initial

---
+++ Mk0.Tools.Calculation/FileSize.cs
@@ -0,0 +1,25 @@
+﻿namespace Mk0.Tools.Calculation
+{
+    public class FileSize
+    {
+        /// <summary>
+        /// Dateigröße in B,KB,MB,TB
+        /// </summary>
+        /// <param name="byteCount">Dateigröße in Bytes</param>
+        /// <returns>Dateigröße in lesbarem Format mit Zusatz (KB, MB...)</returns>
+        public static string GetFileSize(double byteCount)
+        {
+            string size = "0 Bytes";
+            if (byteCount >= 1073741824.0)
+                size = string.Format("{0:##.##}", byteCount / 1073741824.0) + " GB";
+            else if (byteCount >= 1048576.0)
+                size = string.Format("{0:##.##}", byteCount / 1048576.0) + " MB";
+            else if (byteCount >= 1024.0)
+                size = string.Format("{0:##.##}", byteCount / 1024.0) + " KB";
+            else if (byteCount > 0 && byteCount < 1024.0)
+                size = byteCount.ToString() + " Bytes";
+
+            return size;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Mk0.Tools.Calculation
+(C) 2019 mk0.at
+
+Allows the following operations
+
++ GetFileSize - input bytes and recieve f.e. "20,5 MB"
